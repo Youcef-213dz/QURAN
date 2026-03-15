35e46623c6522e6e404a1bba5bb024365e2f1627
Youcef-213dz <161446123+Youcef-213dz@users.noreply.github.com>
2026-03-15 12:57:16 +0100

    Add files via upload

---
+++ data/reciters.js
@@ -0,0 +1,26 @@
+const recitersData = [
+  { 
+    "id": 1, 
+    "name": "أحمد العجمي", 
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJjHyxdHsyxFMlbxv7X0R2G6gDp-XzC94-q5o7T3I-1A0Y7_3d5wZq17mMf_KVT14vJ5Dz5tQzsLkvcVhKvrvJQ-hRxKVMYKTwWoYyP8jKaNltWiOtiQ&s=10&ec=121584920", 
+    "data": ajmData  // لاحظ هنا: بدون " " لكي يأخذ المصفوفة من ملف ajm.js
+  },
+  { 
+    "id": 2, 
+    "name": "سعد الغامدي", 
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxXnTsL1QarHgBdtyvD1FsKc2SoJx0voVJX93LU2q75SyM9W1UGXzUy3jcxELpe_9XtTVnoX9-dow2qFI66rd9-Q7Fm_w7&s&ec=121584920", 
+    "data": gmdData // لاحظ هنا: gmdData وليس ghmdData كما في محاولات سابقة
+  },
+{ 
+    "id": 3, 
+    "name": " عبد الباسط ", 
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhH5zAMCz4omvneDqLon21jX8Tiau2ZUEDz0cgnM4beDK7qyzbkYNN5foxjIKOsZt5YJloybxmg3-bDp6TQu5TcQuOXCcaw_6tJKfI1mgmQXQaVx202k7cGFgLfWAz3X8HKg&s=10&ec=121584920", 
+    "data": basitData  // لاحظ هنا: بدون " " لكي يأخذ المصفوفة من ملف ajm.js
+  },
+  { 
+    "id": 4, 
+    "name": "سعود الشريم ", 
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxCYdwHVgYYFku8NxG0mkgHlhl5oysKhgwIy0iE3EHvq44fudESIhQbO-uSK9uOdb-x2TKRMSPer5Y2OVkxhCXQRX7VKRGE0XDZ8M9Cfpm_ib6r-h-SnyqGvumeRuNiJ3xMg0&s=10&ec=121584920", 
+    "data": shrmData // لاحظ هنا: gmdData وليس ghmdData كما في محاولات سابقة
+  }
+];
